c233eaddedf14f14b14d989ac4d2ac733db6586d
EvgMarkov <evgenjamar@mail.ru>
2023-02-16 22:36:04 +0300

    initial commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+valid_email = "e.andrrreeva@gmail.com"
+valid_password = "poland"
+
+
+invalid_password = '12345'
+invalid_data = 'РШ4KJvAПi%rRоLs)жЦнскGбMPЪфЪюXгQя1DТGz5б^Uщ6ЧТOwtРy' \
+               'ЗlсgA4tйtdш)rЮДЪЩ7uG(qjпxШЪЕoИ^7МеplЕJ@ГgXtr8#буЭSiНYf' \
+               'д*ЫDеГ4.РУЭУ7ЪзСD1rнЬt4Ч1гHВhhча6uouПzакr2EtH!h$=Ы$J5n' \
+               'ICщfгшфaQO*м2ZБшОFГЗ^eqГhШ#^МдмДZLTр8NJаV4Eнлр1еi7Sсpч%' \
+               'МТ*-ZзAрйbжВeЩа!СDOШДю#RЬg7VДazGьыдbQgРeLр67huhjnj'
+invalid_email = '1234@mail.ru'
